3d6ca0582f74600ca5fc4ebbe0417503f49a3cdb
Coty Condry <ccondry@cisco.com>
2019-08-08 12:03:41 -0500

    return empty object instead of null for dcloudSession info
    this should prevent undefined reference errors during loading

--- client/store/modules/dcloud.js
+++ client/store/modules/dcloud.js
@@ -11,7 +11,7 @@ const mutations = {
 }
 
 const getters = {
-  dcloudSession: state => state.session,
+  dcloudSession: state => state.session || {},
   sessionId: (state, getters) => getters.dcloudSession.id,
   datacenter: (state, getters) => getters.dcloudSession.datacenter,
   brandDemoLink (state, getters) {
