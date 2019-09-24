3b35015b8d4a00f7ec8b7069036671dd4791ef68
Coty Condry <ccondry@cisco.com>
2019-09-24 12:27:53 -0500

    remove endpoints - use the ones from API

--- client/store/index.js
+++ client/store/index.js
@@ -6,26 +6,6 @@ import * as getters from './getters'
 import mutations from './mutations'
 import modules from './modules'
 
-const endpoints = {
-  user: '/api/v1/cwcc/user'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  // set up production login/logout endpoints
-  endpoints.login = '/api/v1/auth/login'
-  endpoints.logout = '/api/v1/auth/logout'
-  endpoints.user = '/api/v1/auth/user'
-  endpoints.userDemo = '/api/v1/auth/user/demo'
-} else {
-  // set up development login/logout endpoints
-  endpoints.login = 'http://localhost:3032/api/v1/auth/login'
-  endpoints.logout = 'http://localhost:3032/api/v1/auth/logout'
-  endpoints.user = 'http://localhost:3032/api/v1/auth/user'
-  endpoints.userDemo = 'http://localhost:3032/api/v1/auth/user/demo'
-}
-
-endpoints.vertical = 'https://mm.cxdemo.net/api/v1/verticals?all=true&summary=true'
-
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -37,7 +17,7 @@ const store = new Vuex.Store({
     demoConfigId: 'cwcc',
     isProduction: process.env.NODE_ENV === 'production',
     endpointsLoaded: false,
-    endpoints,
+    endpoints: {},
     pkg,
     working: {
       admin: {},
